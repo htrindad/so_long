9cff0049e902e71cab84ba291396aec8c35264f5
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2024-11-15 19:05:43 +0000

    no README

--- src/flood_fill.c
+++ src/flood_fill.c
@@ -6,13 +6,11 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 15:30:24 by htrindad          #+#    #+#             */
-/*   Updated: 2024/11/15 17:02:07 by htrindad         ###   ########.fr       */
+/*   Updated: 2024/11/15 19:04:31 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-
-static int	g_ff_found;
 
 static int	all_cocs(char **map)
 {
@@ -65,18 +63,18 @@ static size_t	find_px(char **map)
 	return (0);
 }
 
-static void	ff(char **map, size_t y, size_t x)
+static void	ff(char **map, size_t y, size_t x, int *ff_found)
 {
-	g_ff_found += (map[y][x] == 'E' || map[y][x] == 'C');
+	*ff_found += (map[y][x] == 'E' || map[y][x] == 'C');
 	map[y][x] = '1';
 	if (map[y - 1][x] != '1')
-		ff(map, y - 1, x);
+		ff(map, y - 1, x, ff_found);
 	if (map[y + 1][x] != '1')
-		ff(map, y + 1, x);
+		ff(map, y + 1, x, ff_found);
 	if (map[y][x - 1] != '1')
-		ff(map, y, x - 1);
+		ff(map, y, x - 1, ff_found);
 	if (map[y][x + 1] != '1')
-		ff(map, y, x + 1);
+		ff(map, y, x + 1, ff_found);
 }
 
 bool	ff_receiver(char **map)
@@ -84,6 +82,7 @@ bool	ff_receiver(char **map)
 	const size_t	py = find_py(map);
 	const size_t	px = find_px(map);
 	char			**cm;
+	int				ff_found;
 
 	cm = copy_dim(map);
 	if (cm == NULL)
@@ -91,9 +90,10 @@ bool	ff_receiver(char **map)
 		null_ptr();
 		exit(-1);
 	}
-	ff(cm, py, px);
+	ff_found = 0;
+	ff(cm, py, px, &ff_found);
 	free_str(cm);
-	if (g_ff_found != all_cocs(map))
+	if (ff_found != all_cocs(map))
 		return (true);
 	return (false);
 }
